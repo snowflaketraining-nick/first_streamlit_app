7b209bec11aaeab5a26afd502fffaa16c2023cd7
snowflaketraining-nick <131152794+snowflaketraining-nick@users.noreply.github.com>
2023-04-18 14:09:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,7 @@ my_fruit_list = my_fruit_list.set_index("Fruit")
 fruits_selected = stl.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected] 
 
+stl.dataframe(fruits_to_show)
+
 # Display the table on the page.
 stl.dataframe(my_fruit_list)
