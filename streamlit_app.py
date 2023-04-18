e0ecb1527845d35998765a43c72cca881fb2b150
snowflaketraining-nick <131152794+snowflaketraining-nick@users.noreply.github.com>
2023-04-18 10:53:27 +0100

    Update streamlit_app.py
    Added breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as stl
 
 stl.title("My Parents New Healthy Diner")
 
-stl.subheader("Breakfast Menu")
+stl.header("Breakfast Menu")
 
 stl.text("Omega 3 & Blueberry Oatmeal")
+stl.text("Kale, Spinach & Rocket Smoothie")
+stl.text("Hard-Boiled Free-Range Egg")
